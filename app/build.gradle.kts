8a8cbb574e79034756dd4caf7ce06bbd7e726d6e
anhnt.dktech <anhnt@dktechgroup.com>
2025-07-02 18:18:29 +0700

    gfjhfg

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,10 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +49,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.datastore.core)
 }
---
+++ app/src/main/java/com/dktech/baseandroidviewdktech/base/BaseActivity.kt
@@ -0,0 +1,53 @@
+package com.dktech.baseandroidviewdktech.base
+
+import android.app.Activity
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.viewbinding.ViewBinding
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+abstract class BaseActivity<viewBinding: ViewBinding>
+    (val inflater: (LayoutInflater) -> viewBinding): AppCompatActivity() {
+    val binding: viewBinding by lazy {
+        inflater(layoutInflater)
+    }
+    private val loadingDialog by lazy {
+        LoadingDialog(this)
+    }
+    abstract val viewModel: BaseViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT
+        window.addFlags(WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON)
+        setContentView(binding.root)
+        initViewModel()
+        initLoadingDialog()
+        initData()
+        initView()
+        initEvent()
+        initObserver()
+
+    }
+
+    abstract fun initViewModel()
+    abstract fun initData()
+    abstract fun initView()
+    abstract fun initEvent()
+    abstract fun initObserver()
+
+    private fun initLoadingDialog(){
+        viewModel.loadingDialog.onEach {
+            if (it) {
+                loadingDialog.show()
+            } else {
+                loadingDialog.hide()
+            }
+        }.launchIn(lifecycleScope)
+    }
+
+}
